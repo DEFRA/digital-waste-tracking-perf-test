1ce0303a88018d29c77e36f7655c91f4587eab56
Lewis Magri <Lewis@limitless.limits.ltd>
2026-03-19 10:39:37 +0000

    Standardised after repsonse logging acrosss the tests when debugging

---
+++ scripts/logging/after-response-log-hook.groovy
@@ -0,0 +1,22 @@
+
+def debug = props.get("debug")?.toString()?.equalsIgnoreCase("true")
+
+if (debug && (prev.getResponseCode().startsWith("4") || prev.getResponseCode().startsWith("5"))) {
+    logAndPrint("================================================================================")
+    logAndPrint("[${prev.getResponseCode()}] ${prev.getSampleLabel()} - ${prev.getResponseCode()} (${prev.getTime()}ms)")
+    logAndPrint("URL: ${prev.getUrlAsString()}")
+    logAndPrint("--- REQUEST HEADERS ---")
+    logAndPrint(prev.getRequestHeaders() ?: "(empty)")
+    logAndPrint("--- REQUEST BODY ---")
+    logAndPrint(prev.getSamplerData() ?: "(empty)")
+    logAndPrint("--- RESPONSE HEADERS ---")
+    logAndPrint(prev.getResponseHeaders() ?: "(empty)")
+    logAndPrint("--- RESPONSE BODY ---")
+    logAndPrint(prev.getResponseDataAsString() ?: "(empty)")
+    logAndPrint("================================================================================")
+}
+
+def logAndPrint(logLine) {
+    log.info(logLine)
+    println(logLine)
+}
